de2306cae18ae1117e20acb3c1bb52d5442364b2
ZhangFuYuan <66827596+ZhangFuYuan69@users.noreply.github.com>
2023-01-24 15:38:27 +0800

    Update 7.3_Rna_Similarity.py

--- 7/7.3_Rna_Similarity.py
+++ 7/7.3_Rna_Similarity.py
@@ -8,23 +8,10 @@ seq2 = 'ACCGUUCU'
 RNA_similarity = 0.0
 b1 = 0
 b2 = 0
+local = {"A": 1 , "G": 2 , "C": 3 , "U": 4 } 
 for base1, base2 in zip(seq1, seq2):
-	if base1 == 'A':
-		b1 = 1
-	elif base1 == 'G':
-		b1 = 2
-	elif base1 == 'C':
-		b1 = 3
-	elif base1 == 'U':
-		b1 = 4
-	if base2 == 'A':
-		b2 = 1
-	elif base2 == 'G':
-		b2 = 2
-	elif base2 == 'C':
-		b2 = 3
-	elif base2 == 'U':
-		b2 = 4
+	b1 = local[base1]
+	b2 = local[base2]
 	RNA_similarity += float(table[b1][b2])
 	print(RNA_similarity)
 print(RNA_similarity)
