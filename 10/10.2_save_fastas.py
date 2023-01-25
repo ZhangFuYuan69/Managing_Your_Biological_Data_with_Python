3ff22c845d69b87af797aea6a23a7ab9b3dd2483
ZhangFuYuan <66827596+ZhangFuYuan69@users.noreply.github.com>
2023-01-25 15:08:04 +0800

    Update 10.2_save_fastas.py

--- 10/10.2_save_fastas.py
+++ 10/10.2_save_fastas.py
@@ -8,17 +8,19 @@ def save_fastas(ac):
 	outf = open(10.2/(ac[0] + '.fasta'), 'w')
 	outf.write(str(sequence))
 	outf.close()
-	i += 1
 
 for line in inf:
 	if line[0] == '>' and flag == 1:
-		ac.append(line.strip().split('|')[1])
 		ac.append(seq)
+		save_fastas(ac)
 		seq = ''
 		ac = []
-		save_fastas(ac)
+		ac.append(line.strip().split('|')[1])
 	else:
 		seq += line.strip()
 		flag = 1
+else:
+	ac.append(seq)
+	save_fastas(ac)
 #print(AC)
 
